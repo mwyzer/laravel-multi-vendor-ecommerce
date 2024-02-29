2e5b13de5cf950c76996d345fa39ffef703019ac
mwyzer <muhammad.wyzer@gmail.com>
2024-02-29 16:39:23 +0700

    add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+
+@extends('layouts.main')
+
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,67 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route Grouping
+
+
+// Route::group(['prefix' => 'customer'], function() {
+//     Route::get('/', function() {
+//         return "<h1>Customer List</h1>";
+//     });
+
+//     Route::get('/create', function() {
+//         return "<h1>Customer Create</h1>";
+//     });
+
+//     Route::get('/show', function() {
+//         return "<h1>Customer Show</h1>";
+//     });
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+/* Passing and Rendering */
+Route::get('/about', function () {
+    $a = 1 + 1;
+    $b = "wives";
+    // return view('about', ['about' => $about ]);
+    return view('about', compact('a', 'b')); // ['about => $about]
+});
+Route::get('/contact', function () {
+    return "<h1>Contact Page</h1>";
+});
+
+/* Fallback Route */
+Route::fallback(function(){
+    return "Route not exists";
+});
+
+
+
+
+
+
+
+
+// Route::get('about', function() {
+//     return "<h1>Contact Page</h1>";
+// });
+
+// Route::get('contact', function () {
+//     return view('<h1>Contact Page</h1>');
+// });
+
+// Route::get('contact/{id}', function ($id) {
+//     return $id;
+// })->name('edit-contact');
+
+
+// Route::get('home', function() {
+//     return "<a href='".route('edit-contact', 1)."'>About</a>";
+// });
+
+
+
